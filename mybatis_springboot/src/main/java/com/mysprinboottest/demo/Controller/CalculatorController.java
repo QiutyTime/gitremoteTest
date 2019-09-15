9994a72e921ca81fe546a44f46b20b1db6b8cb64
QiutyTime <Qiutyce@hotmail.com>
2019-09-15 20:06:16 +0800

    完成了计算器的简单实现

--- mybatis_springboot/src/main/java/com/mysprinboottest/demo/Controller/CalculatorController.java
+++ mybatis_springboot/src/main/java/com/mysprinboottest/demo/Controller/CalculatorController.java
@@ -2,7 +2,7 @@ package com.mysprinboottest.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 /**
@@ -21,14 +21,39 @@ public class CalculatorController {
     @RequestMapping("/a")
     public String test()
     {
+
         return "views/start";
     }
 
     @RequestMapping("/b")
-    public String test1( Map<String,Object> map)
+    public String test1( Map<String,Object> map,@RequestParam("firstNumber") double firstNumber,
+                         @RequestParam("secondNumber") double secondNumber,@RequestParam("radio") double radio)
     {
-        map.put("name","2");
+        System.out.println(radio);
+        double anwser = 0.0;
+        switch ((int) radio) {
+            case 1:
+                anwser = firstNumber + secondNumber;
+                break;
+            case 2:
+                anwser = firstNumber - secondNumber;
+                break;
+            case 3:
+                anwser = firstNumber * secondNumber;
+                break;
+            case 4:
+                anwser = firstNumber / secondNumber;
+                break;
+            default:
+                break;
+        }
+
+
+        map.put("answer", anwser);
+
 
         return "views/answer";
+
+
     }
 }
